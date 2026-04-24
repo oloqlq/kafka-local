ae816d20ce5528aacdfe8a98fba916a868baf1bc
oloqlq <oloqlqxown0919@gmail.com>
2026-04-24 13:43:12 +0900

    Feat : 그냥 다 새로

--- log_gen.py
+++ log_gen.py
@@ -1,43 +1,52 @@
-##############################
-#  로그 생성 -> 파일에 기록(json) #
-##############################
+'''
+- 로그 생성 => 파일 기록
+- json 형태, 텍스트(한줄에 로그기록 작성) 형태
+'''
 
-# 1. 패키지 호출
+# 1. 모듈 가져오기
 import json
 import time
 import datetime
 import os
 
-# 2. 로그 저장 디렉토리 생성
+# 2. 로그가 저장되는 디렉토리 지정/생성..
 log_dir = './sensor_logs'
-if not os.path.exists(log_dir):
+if not os.path.exists(log_dir): # 없으면 -> 생성
     os.makedirs(log_dir)
 
 # 3. 로그 발생 및 저장
 def generate_logs():
+    # 로그 샘플 (고정된 장비 1대로 지정)
+    # 시간을 제외한 모든 값은 고정했음 -> 편의상
     data = {
-        "timestamp"     : datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        "sensor_id"     : "AI-FACTORY-001",
-        "temperature"   : 87.5, 
-        "humidity"      : 41.1, 
-        "status"        : "RUNNING"
+        "timestamp" : datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "sensor_id" : "AI-FACTORY-001",
+        "temperature" : 87.5,
+        "humidity"  : 42.1,
+        "status"    : "RUNNING"
     }
-    # json 형태로 기록 : dict 직렬화 처리
-    with open(f"{log_dir}/sensor_json.log", "a", encoding="utf-8") as f:  # "a" : append; 파일에 덧붙여서 쓰기 모드
-        f.write( json.dumps(data) + "\n" )
-    # text 형태로 기록 -> f-string구성
-    text = f"[{ data["timestamp"] }] ID={data["sensor_id"]} |     TEMP:{data["temperature"]} |   HUMI:{data["humidity"]} |    STAT:{data["status"]}"
-    with open(f"{log_dir}/sensor_text.log", "a", encoding="utf-8") as f:
-        f.write(json.dumps(text) + "\n")
-    
-    print(f"로그발생완료 {data["timestamp"]}")
 
-# 4. 로그발생기 가동
+    # json 형태로 파일 기록(한줄에 로그 1개씩) -> dict 객체의 직렬화 처리
+    # 파일명 ./sensor_logs/sensor_json.log
+    # 한줄에 JSON 객체 1개씩 문자열로 기록(JSON Lines : JSONL)
+    # 구현하시오, a : Append 파일에 추가 쓰기 모드 -> 파일에 끝에 새로운 내용을 덧붙임
+    with open( f"{log_dir}/sensor_json.log", "a", encoding="utf-8" ) as f:
+        f.write( json.dumps(data) + "\n" )
+
+    # text 형태로 파일 기록(한줄에 로그 1개씩) -> f-string 구성
+    # 파일명 ./sensor_logs/sensor_text.log
+    text = f"[{ data["timestamp"] }] ID={data["sensor_id"]} |   TEMP:{data["temperature"]} |   HUMI:{data["humidity"]} |   STAT:{data["status"]}"
+    with open( f"{log_dir}/sensor_text.log", "a", encoding="utf-8" ) as f:
+        f.write( json.dumps(text) + "\n" )
+    
+    print(f"로그 발생 완료 { data["timestamp"] }")
+
+# 4. 로그 발생기 가동
 def main():
     try:
         while True:
             generate_logs()
-            time.sleep(2)
+            time.sleep(2) # 2초 대기(임의 설정)
     except Exception:
         print('종료 완료')
     pass
@@ -46,4 +55,3 @@ def main():
 if __name__ == '__main__':
     print('로그 발생 시작. 종료 ctrl+c')
     main()
-
